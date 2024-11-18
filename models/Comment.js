556ea008d1676c3a07c1a682613db4fc9368b2de
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-18 10:07:17 -0500

    Created a comment with Body/CreatedAt

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,23 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../db/connection');
 
-let Comment;
+class Comment extends Model {}
 
+Comment.init(
+  {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Comment',
+  }
+);
 
 module.exports = Comment;
