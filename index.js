dfceb8cbb70c870d9cafcb86d286212c8442e7ca
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-18 10:28:46 -0500

    Creation to set conditions for models

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const { Comment, Like, Post, Profile, User } = require("./models/index");
+const sequelize = require('./db/connection');
+const User = require('./models/User');
+const Profile = require('./models/Profile');
+const Post = require('./models/Post');
+const Comment = require('./models/Comment');
+const Like = require('./models/Like');
 
-// Define your associations here
+User.hasOne(Profile);
+Profile.belongsTo(User);
 
+User.hasMany(Post);
+Post.belongsTo(User);
 
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
 
+User.hasMany(Like);
+Like.belongsTo(User);
 
-
-module.exports = {
-    Comment,
-    Like,
-    Post,
-    Profile,
-    User
-}
+sequelize.sync({ force: true })
+  .then(() => {
+    console.log('Database synced successfully!');
+  })
+  .catch((err) => {
+    console.error('Error syncing database:', err);
+  });
