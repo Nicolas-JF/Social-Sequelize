4ba2d18c5fbb85af6843943342467e59f1baa6cd
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-18 10:03:15 -0500

    Profile defined with Bio/ProfilePicture/Birthday

--- models/Profile.js
+++ models/Profile.js
@@ -1,5 +1,29 @@
+// models/Profile.js
 
-let Profile;
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../db/connection');
 
+class Profile extends Model {}
+
+Profile.init(
+  {
+    bio: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    birthday: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Profile',
+  }
+);
 
 module.exports = Profile;
